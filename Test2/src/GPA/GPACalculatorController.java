4a2c40b8e1a8f6099c870ff8d93eccafc33613b0
Sam Swanson <tehswanny7@gmail.com>
2019-10-01 17:25:57 -0500

    GPA Calculator Interface
    Calculator interface is complete and algorithm is written in pseudocode

--- Test2/src/GPA/GPACalculatorController.java
+++ Test2/src/GPA/GPACalculatorController.java
@@ -1,4 +1,32 @@
 package GPA;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
+
 public class GPACalculatorController {
+    // algorithym for calculating GPA
+    //(4 credit hours * completed((4*a)/(3*b)/(2*c)/(1*d)/(0*f)) + 3 credit hours * completed((4*a)/(3*b)/(2*c)/(1*d)/(0*f))
+    // + 2 credit hours * completed((4*a)/(3*b)/(2*c)/(1*d)/(0*f)) + 1 credit hours * completed((4*a)/(3*b)/(2*c)/(1*d)/(0*f))) = score
+    // score/total credit hours = GPA
+/*
+    @FXML
+    public Button backButton;
+
+    public void gpaBack(ActionEvent actionEvent) throws IOException {
+        Stage appStage;
+        Parent root;
+        appStage=(Stage) backButton.getScene().getWindow();
+        root= FXMLLoader.load(getClass().getResource("/homePage/home.fxml"));
+        Scene scene=new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }*/
 }
--- Test2/src/Student/StudentController.java
+++ Test2/src/Student/StudentController.java
@@ -1,8 +1,27 @@
 package Student;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StudentController {
 
+    @FXML
+    public Button backButton;
+
+    public void studentBack(ActionEvent actionEvent) throws IOException {
+        Stage appStage;
+        Parent root;
+        appStage=(Stage) backButton.getScene().getWindow();
+        root= FXMLLoader.load(getClass().getResource("/homePage/home.fxml"));
+        Scene scene=new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
 }
