1b21d78515005fe58fdba510444c335e5be0773f
Charlie Orch <37132986+charlieorch@users.noreply.github.com>
2019-10-01 15:56:13 -0500

    Login Page Push

--- Test2/src/login/LoginPageController.java
+++ Test2/src/login/LoginPageController.java
@@ -8,12 +8,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import javax.xml.namespace.QName;
 import java.io.IOException;
 
 public class LoginPageController {
 
     @FXML
     public Button backButton;
+    public Button enterButton;
+
+    public void enterHomepage(ActionEvent actionEvent) throws IOException {
+        Stage appStage;
+        Parent root;
+        appStage=(Stage) enterButton.getScene().getWindow();
+        root= FXMLLoader.load(getClass().getResource("/homePage/home.fxml"));
+        Scene scene=new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
 
     public void loginBack(ActionEvent actionEvent) throws IOException {
         Stage appStage;
