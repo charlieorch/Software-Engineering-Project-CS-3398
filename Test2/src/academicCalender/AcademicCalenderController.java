c25b2d56ef917874184dea09d29d41b9ea503544
Sam Swanson <tehswanny7@gmail.com>
2019-10-01 12:56:04 -0500

    Homepage Design
    Made a rough version of what the homepage can look like

--- Test2/src/academicCalender/AcademicCalenderController.java
+++ Test2/src/academicCalender/AcademicCalenderController.java
@@ -1,5 +1,11 @@
 package academicCalender;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AcademicCalenderController {
-    
+
+    @FXML
+    public Button button;
+
 }
