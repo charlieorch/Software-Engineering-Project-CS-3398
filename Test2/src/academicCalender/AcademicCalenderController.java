cabfcebcc35c26730309de90a7effd88b462d064
es362 <54688732+es362@users.noreply.github.com>
2019-10-01 18:13:21 -0500

    Academic Calender

--- Test2/src/academicCalender/AcademicCalenderController.java
+++ Test2/src/academicCalender/AcademicCalenderController.java
@@ -1,11 +1,28 @@
 package academicCalender;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AcademicCalenderController {
 
     @FXML
-    public Button button;
+    public Button backButton;
+
+    public void backButtonPress(ActionEvent actionEvent) throws IOException {
+        Stage appStage;
+        Parent root;
+        appStage=(Stage) backButton.getScene().getWindow();
+        root= FXMLLoader.load(getClass().getResource("/homePage/home.fxml"));
+        Scene scene=new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
 
 }
