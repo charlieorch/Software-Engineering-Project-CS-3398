78acc27230a1edb07d4d57e5548df6ea3c0397af
MesutApil <mesutapilts@gmail.com>
2019-10-03 18:11:57 -0500

    Grade calculator interface
    Grade calculator interface

--- Test2/src/Grade/GradeController.java
+++ Test2/src/Grade/GradeController.java
@@ -1,4 +1,27 @@
 package Grade;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import options.OptionsPageController;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
+
 public class GradeController {
+
+    @FXML
+    public Button backButton;
+
+    public void gradeBack(ActionEvent actionEvent) throws IOException {
+
+    }
+
+
+
+
 }
